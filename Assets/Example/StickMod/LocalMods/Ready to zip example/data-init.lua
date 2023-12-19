11a0fdc4844111e4dd125cb71d8ac0c0c374af6a
Noah Bannister <noahabannister@gmail.com>
2023-12-19 17:33:08 -0500

    changed render pipeline to match game project

---
+++ Assets/Example/StickMod/LocalMods/Ready to zip example/data-init.lua
@@ -0,0 +1,8 @@
+local stick = {
+ prefabAddress = 'Assets/Example/StickMod/Prefabs/Stick.prefab',
+ displayName = 'Stick',
+ weight = 50,
+ melee_damage = 200
+}
+
+weaponTable[stick.prefabAddress] = stick
