2f80d3f912a08a55eca9dedd8015b8b90341977c
Noah Bannister <noahabannister@gmail.com>
2024-04-24 17:01:01 -0400

    Added Frenzy Extinction Pistol as example

---
+++ Assets/Example/ExtinctionWeaponsPack/ExportContent/data-init.lua
@@ -0,0 +1,8 @@
+local extinctionPistol = {
+ prefabAddress = 'Assets/Example/ExtinctionWeaponsPack/Prefabs/ExtinctionPistol.prefab',
+ displayName = 'Scrap Pistol',
+ weight = 50,
+ melee_damage = 200
+}
+
+weaponTable[extinctionPistol.prefabAddress] = extinctionPistol
